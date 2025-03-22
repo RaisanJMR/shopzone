de58ff3d92cff9d30c0b6619565c40733e4e937b
RaisanJMR <raisanjmr@protonmail.com>
2025-03-22 20:01:32 +0530

    add Footer component to CartScreen

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import CartItem from '../components/CartItem';
 import { useCart } from '../hooks/useCart';
 
@@ -29,6 +30,7 @@ const CartScreen = () => {
                     </div>
                 )}
             </main>
+            <Footer/>
         </div>
     );
 };
